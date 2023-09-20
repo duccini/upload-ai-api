4ae2ee7bd9a810d1976705adbce8686f8e5804c7
Guilherme Duccini <guilhermeduccini@gmail.com>
2023-09-20 18:44:07 -0300

    upload ai api finished

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -4,24 +4,50 @@ import { z } from "zod";
 import { createReadStream } from "node:fs";
 import { openai } from "../lib/openai";
 
-// a transcrição será feita de um video já carregado previamente
-export async function createTranscriptionRoute(app: FastifyInstance) {
-  app.post("/videos/:videoId/transcription", async (req, res) => {
-    // Definindo o formato de params e o que esperamos que tenha dentro dele
-    const paramsSchema = z.object({
-      // string no formato uuid, que é salvo no BD
-      videoId: z.string().uuid({ message: "Invalid UUID" }),
-    });
-
-    // o parse valida, ou nao, se params está seguindo o formato de paramsSchema
-    // se seguir, retorna o objeto
-    const { videoId } = paramsSchema.parse(req.params);
-
-    // alem do video para transcrição, inserimos o prompt no corpo da requisição
+export async function generateAiCompleteRoute(app: FastifyInstance) {
+  app.post("/generate", async (req, reply) => {
     const bodySchema = z.object({
-      prompt: z.string(),
+      videoId: z.string().uuid({ message: "Invalid UUID" }),
+      /**
+       * no frontend o compo pronpt seleciona um dos templates cadastrados no BD
+       * como o usuário pode editar esses templates, enviamos para esta rota o texto do template já
+       * editado pelo usuário e não o ID do template
+       */
+      editTemplate: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
     });
 
-    const { prompt } = bodySchema.parse(req.body);
+    // ATENÇÃO: os dados vem do BODY e não da rota( PARAMS )
+    const { videoId, editTemplate, temperature } = bodySchema.parse(req.body);
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    // o video precisa da transcrição, se nao existir retornar erro
+    if (!video.transcription) {
+      return reply
+        .status(400)
+        .send({ error: "Video transcription was not generated yet." });
+    }
+
+    const promptMessage = editTemplate.replace(
+      "{transcription}",
+      video.transcription
+    );
+
+    const response = await openai.chat.completions.create({
+      // Open AI tokes = tokens enviados (aprox. 1 a cada 4 palavras + tokens recebidos)
+      model: "gpt-3.5-turbo-16k",
+      temperature,
+      messages: [
+        // poderia ser varias mensagens como um historico
+        { role: "user", content: promptMessage },
+      ],
+    });
+
+    return response;
   });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompleteRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
+
+// Set Cors
+// qualquer url pode acessar o backend
+// ideal: em produção colocar o end de onde esta hospedado o backend
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 // cadastrar a rota no app
 // todos os modulos cadastrados pelo registor precisam ser async
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAiCompleteRoute);
 
 app
   .listen({
